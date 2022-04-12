d938008630adc5b8eba658ebdfbc52d00b4a09e6
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-04-12 20:44:31 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var translateButton = document.querySelector("#translate-button");
+var textInput = document.querySelector("#translate-text-holder");
+var textOutput = document.querySelector("#text-output-holder");
+var selectOption = document.getElementById("language-selector");
+translateButton.addEventListener("click", acknowledgeClick);
+
+const serverUrl = "https://api.funtranslations.com/translate/"
+const testServerUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function acknowledgeClick() {
+  console.log(selectOption.value);
+  if (textInput.value != "" && selectOption.value != "testing") {
+  var toBeTranslated = serverUrl + selectOption.value + ".json?text=" + textInput.value;
+  fetch(toBeTranslated)
+    .then(response => response.json())
+    .then(data => textOutput.innerText = data.contents.translated)
+  }
+}
