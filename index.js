58b2a0c1b42b0976cab74696002d1ac0f5c3913b
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-04-19 00:17:14 +0530

    feat: added insult generator bits

--- index.js
+++ index.js
@@ -1,26 +1,71 @@
 var translateButton = document.querySelector("#translate-button");
 var textInput = document.querySelector("#translate-text-holder");
 var textOutput = document.querySelector("#text-output-holder");
-var selectOption = document.getElementById("language-selector");
-translateButton.addEventListener("click", acknowledgeClick);
+let selectOption = document.getElementById("character-selector");
+let insultLanguage = document.querySelector("#insultlanguage");
+let translateServerUrl = `https://api.funtranslations.com/translate/${selectOption.value}.json?text=${textInput.value}`;
+let insultUrl = `https://evilinsult.com/generate_insult.php?lang=${insultLanguage.value}&type=json`;
+let testServerUrl = `https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textInput.value}`;
 
-const serverUrl = "https://api.funtranslations.com/translate/"
-const testServerUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+function printToDiv (text){
+  textOutput.innerText = text;
+}
+
+function fetchData (serverUrl) {
+  fetch(serverUrl);
+}
 
 function acknowledgeClick() {
-  console.log(selectOption.value);
-  if (textInput.value != "" && selectOption.value != "testing") {
-  var toBeTranslated = serverUrl + selectOption.value + ".json?text=" + textInput.value;
-  fetch(toBeTranslated)
-    .then(response => response.json())
-    .then(data => textOutput.innerText = data.contents.translated)
-  } else if (selectOption.value === "testing" ) {
-    var toBeTranslated = testServerUrl + "?text=" + textInput.value;
-    fetch(toBeTranslated)
-    .then(response => response.json())
-    .then(data => textOutput.innerText = data.contents.translated)
-  }
-  else {
-    textOutput.innerText = "Error: Text is missing!"
+  printToDiv(fetchData(testServerUrl, textInput.value));
+}
+
+function insultcheck() {
+
+  console.log(typeof selectOption.value);
+  if (selectOption.value === "insultplease") {
+    textInput.innerText = "";
+    document.querySelector("#insult-language-select-form").setAttribute("style", "display: block;");
+    textInput.setAttribute("style", "display: none;")
+    document.querySelector("#character-selector-label").innerText = "I want to :";
+    translateButton.innerText = "Generate Insult"
   }
 }
+
+
+// function acknowledgeClick() {
+//   console.log(selectOption.value);
+//   switch (selectOption.value) {
+//     case "testing":
+//       fetch(testServerUrl)
+//       .then(response => response.json())
+//       .then(data => textOutput.innerText = data.contents.translated)
+//       break;
+  
+//     default:
+
+//       break;
+//   }
+  // if (selectOption.value === "testing" && textInput.value != "") {
+  //   var toBeTranslated = testServerUrl + "?text=" + textInput.value;
+  //   fetch(toBeTranslated)
+  //   .then(response => response.json())
+  //   .then(data => textOutput.innerText = data.contents.translated)
+  // } else if (selectOption === "insultplease" && textInput.value != "") {
+  //   fetch(insultUrl)
+  //   .then(response => response.json())
+  //   .then(data => textOutput.innerText = data.contents.insult)
+  // } else if (selectOption.value != "testing" && selectOption.value != "insultplease" && textInput.value != "") {
+  //   var toBeTranslated = serverUrl + selectOption.value + ".json?text=" + textInput.value;
+  //   fetch(toBeTranslated)
+  //     .then(response => response.json())
+  //     .then(data => textOutput.innerText = data.contents.translated)
+  // } else {
+  //   textOutput.innerText = "Error: Text is missing! (╯°□°)╯︵ ┻━┻"
+  // }
+// }
+
+
+
+// console.log(fetch())
+
+// translateButton.addEventListener("click", acknowledgeClick);
